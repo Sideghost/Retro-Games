03740fbbf40a27b19e181b328d8cf69b9400419b
Sideghost <guilherme.cordeiro.pereira@gmail.com>
2024-03-27 17:00:24 +0000

    Dependencies altered from lib update

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    flatDir {
-        dirs("libs")
-    }
 }
 
 dependencies {
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    implementation("pt.isel:CanvasLib-jvm:1.0.1")
+    implementation("io.github.palex65:CanvasLib-jvm:1.0.2")
 }
 
 tasks.test {
